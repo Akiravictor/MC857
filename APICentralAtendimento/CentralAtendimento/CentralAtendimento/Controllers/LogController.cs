8cfff1530240633be1bb466ea11e7a7044853592
akira <victor.akira.hs@gmail.com>
2018-04-02 01:29:29 -0300

    WebAPI Done and published!

--- APICentralAtendimento/CentralAtendimento/CentralAtendimento/Controllers/LogController.cs
+++ APICentralAtendimento/CentralAtendimento/CentralAtendimento/Controllers/LogController.cs
@@ -6,193 +6,293 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Data.Entity;
+using System.Web.Http.Description;
 
 namespace CentralAtendimento.Controllers
 {
-    public class LogController : ApiController
-    {
-        private APIDbContext db;
+	public class LogController : ApiController
+	{
+		private APIDbContext db;
 
-        public LogController()
-        {
-            db = new APIDbContext();
-        }
+		public LogController()
+		{
+			db = new APIDbContext();
+		}
 
-        protected override void Dispose(bool disposing)
-        {
-            db.Dispose();
-        }
+		protected override void Dispose(bool disposing)
+		{
+			db.Dispose();
+		}
 
-        [HttpGet]
-        [Route("tickets/{siteId}/{clienteId}")]
-        public Logs GetAllTickets(string siteId, string clienteId)
-        {
-            Logs logResponse = new Logs();
+		[HttpGet]
+		[Route("tickets/{siteId}/{clienteId}")]
+		public Logs GetAllTickets(string siteId, string clienteId)
+		{
+			Logs logResponse = new Logs();
 
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                var tkts = db.TicketsDb.Include(n => n.MessagesList).Where(n => n.ClienteId == clienteId).ToList();
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				var tkts = db.TicketsDb.Include(n => n.MessagesList)
+											.Where(t => t.ClienteId == clienteId)
+											.Where(t => t.SiteId == siteId).ToList();
 
-                logResponse.TicketSize = tkts.Count;
-                logResponse.TicketsList = tkts;
+				logResponse.TicketSize = tkts.Count;
+				logResponse.TicketsList = tkts;
 
-                foreach (var tkt in tkts)
-                {
-                    tkt.MessageSize = tkt.MessagesList.Count;
-                }
-            }
+				foreach (var tkt in tkts)
+				{
+					tkt.MessageSize = tkt.MessagesList.Count;
+				}
+			}
 
-            return logResponse;
-        }
+			return logResponse;
+		}
 
-        [HttpGet]
-        [Route("tickets/{siteId}/{clienteId}/compra/{compraId}")]
-        public Logs GetTicketsByPurchase(string siteId, string clienteId, string compraId)
-        {
-            Logs logResponse = new Logs();
+		[HttpGet]
+		[Route("tickets/{siteId}/{clienteId}/compra/{compraId}")]
+		public Logs GetTicketsByPurchase(string siteId, string clienteId, string compraId)
+		{
+			Logs logResponse = new Logs();
 
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                var tkts = db.TicketsDb.Include(n => n.MessagesList).Where(n => n.ClienteId == clienteId).Where(a => a.CompraId == compraId).ToList();
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				var tkts = db.TicketsDb.Include(n => n.MessagesList)
+											.Where(t => t.ClienteId == clienteId)
+											.Where(t => t.CompraId == compraId)
+											.Where(t => t.SiteId == siteId).ToList();
 
-                logResponse.TicketSize = tkts.Count;
-                logResponse.TicketsList = tkts;
+				logResponse.TicketSize = tkts.Count;
+				logResponse.TicketsList = tkts;
 
-                foreach (var tkt in tkts)
-                {
-                    tkt.MessageSize = tkt.MessagesList.Count;
-                }
-            }
+				foreach (var tkt in tkts)
+				{
+					tkt.MessageSize = tkt.MessagesList.Count;
+				}
+			}
 
-            return logResponse;
-        }
+			return logResponse;
+		}
 
-        [HttpGet]
-        [Route("tickets/{siteId}/{clienteId}/ticket/{ticketId}")]
-        public Logs GetTicketsByTicket(string siteId, string clienteId, string ticketId)
-        {
-            Logs logResponse = new Logs();
+		[HttpGet]
+		[Route("tickets/{siteId}/{clienteId}/ticket/{ticketId}")]
+		public Logs GetTicketsByTicket(string siteId, string clienteId, string ticketId)
+		{
+			Logs logResponse = new Logs();
 
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                var tkts = db.TicketsDb.Include(n => n.MessagesList).Where(n => n.ClienteId == clienteId).Where(a => a.TicketId == ticketId).ToList();
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				var tkts = db.TicketsDb.Include(n => n.MessagesList)
+										   .Where(t => t.ClienteId == clienteId)
+										   .Where(t => t.TicketId == ticketId)
+										   .Where(t => t.SiteId == siteId).ToList();
 
-                logResponse.TicketSize = tkts.Count;
-                logResponse.TicketsList = tkts;
+				logResponse.TicketSize = tkts.Count;
+				logResponse.TicketsList = tkts;
 
-                foreach (var tkt in tkts)
-                {
-                    tkt.MessageSize = tkt.MessagesList.Count;
-                }
-            }
+				foreach (var tkt in tkts)
+				{
+					tkt.MessageSize = tkt.MessagesList.Count;
+				}
+			}
 
-            return logResponse;
-        }
+			return logResponse;
+		}
 
-        [HttpPost]
-        [Route("tickets/{siteId}/{clienteId}")]
-        public HttpStatusCode CreateGenericTicket(string siteId, string clienteId, Messages msg)
-        {
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                if (ModelState.IsValid)
-                {
-                    Tickets tktToDb = new Tickets();
-                    Messages msgToDb = new Messages();
+		[HttpPost]
+		[Route("tickets/{siteId}/{clienteId}")]
+		[ResponseType(typeof(SystemMessages))]
+		public HttpResponseMessage CreateGenericTicket(HttpRequestMessage request, string siteId, string clienteId, Messages msg)
+		{
+			SystemMessages sysMsg = new SystemMessages();
 
-                    tktToDb.TicketId = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString();
-                    tktToDb.ClienteId = clienteId;
-                    tktToDb.SiteId = siteId;
-                    tktToDb.StatusId = Status.Open;
-                    tktToDb.MessagesList = new List<Messages>();
-                    tktToDb.MessagesList.Add(msg);
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				if (ModelState.IsValid)
+				{
+					Tickets tktToDb = new Tickets();
+					Messages msgToDb = new Messages();
 
-                    db.TicketsDb.Add(tktToDb);
-                    db.MessagesDb.Add(msg);
+					tktToDb.TicketId = DateTime.Now.Year.ToString("yyyy")
+										+ DateTime.Now.Month.ToString("MM")
+										+ DateTime.Now.Day.ToString("dd")
+										+ DateTime.Now.Hour.ToString("HH")
+										+ DateTime.Now.Minute.ToString("mm")
+										+ DateTime.Now.Second.ToString("ss");
 
-                    db.SaveChanges();
+					tktToDb.ClienteId = clienteId;
+					tktToDb.SiteId = siteId;
+					tktToDb.StatusId = Status.Open;
+					tktToDb.MessagesList = new List<Messages>();
+					tktToDb.MessagesList.Add(msg);
 
-                    return HttpStatusCode.Created;
-                }
+					db.MessagesDb.Add(msg);
+					db.TicketsDb.Add(tktToDb);
 
-                return HttpStatusCode.BadRequest;
-            }
-            else
-            {
-                return HttpStatusCode.BadRequest;
-            }
-        }
+					db.SaveChanges();
 
-        [HttpPost]
-        [Route("tickets/{siteId}/{clienteId}/compra/{compraID}")]
-        public HttpStatusCode CreateTicketForPurchase(string siteId, string clienteId, string compraID, Messages msg)
-        {
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                if (ModelState.IsValid)
-                {
-                    Tickets tktToDb = new Tickets();
-                    Messages msgToDb = new Messages();
+					sysMsg.SystemMessage = "Ticket created Successfully!";
+					return request.CreateResponse(HttpStatusCode.Created, sysMsg);
+				}
 
-                    tktToDb.TicketId = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString();
-                    tktToDb.ClienteId = clienteId;
-                    tktToDb.CompraId = compraID;
-                    tktToDb.SiteId = siteId;
-                    tktToDb.StatusId = Status.Open;
-                    tktToDb.MessagesList = new List<Messages>();
-                    tktToDb.MessagesList.Add(msg);
+				sysMsg.SystemMessage = "Request malformed!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+			else
+			{
+				sysMsg.SystemMessage = "Site key provided not recognized!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+		}
 
-                    db.TicketsDb.Add(tktToDb);
-                    db.MessagesDb.Add(msg);
+		[HttpPost]
+		[Route("tickets/{siteId}/{clienteId}/compra/{compraID}")]
+		[ResponseType(typeof(SystemMessages))]
+		public HttpResponseMessage CreateTicketForPurchase(HttpRequestMessage request, string siteId, string clienteId, string compraID, Messages msg)
+		{
+			SystemMessages sysMsg = new SystemMessages();
 
-                    db.SaveChanges();
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				if (ModelState.IsValid)
+				{
+					Tickets tktToDb = new Tickets();
+					Messages msgToDb = new Messages();
 
-                    return HttpStatusCode.Created;
-                }
+					tktToDb.TicketId = DateTime.Now.Year.ToString("yyyy")
+										+ DateTime.Now.Month.ToString("MM")
+										+ DateTime.Now.Day.ToString("dd")
+										+ DateTime.Now.Hour.ToString("HH")
+										+ DateTime.Now.Minute.ToString("mm")
+										+ DateTime.Now.Second.ToString("ss");
 
-                return HttpStatusCode.BadRequest;
-            }
-            else
-            {
-                return HttpStatusCode.BadRequest;
-            }
-        }
+					tktToDb.ClienteId = clienteId;
+					tktToDb.CompraId = compraID;
+					tktToDb.SiteId = siteId;
+					tktToDb.StatusId = Status.Open;
+					tktToDb.MessagesList = new List<Messages>();
+					tktToDb.MessagesList.Add(msg);
 
-        [HttpPut]
-        [Route("tickets/{siteId}/{clienteId}/id/{ticketID}")]
-        public HttpStatusCode AddMsgToTicket(string siteId, string clienteId, string ticketId, Messages msg)
-        {
-            if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
-            {
-                if (ModelState.IsValid)
-                {
-                    var tktToDb = db.TicketsDb.Include(n => n.MessagesList).Where(n => n.ClienteId == clienteId).Where(a => a.TicketId == ticketId);
+					db.MessagesDb.Add(msg);
+					db.TicketsDb.Add(tktToDb);
 
-                    tktToDb.MessagesList.Add(msg);
-                    db.MessagesDb.Add(msg);
+					db.SaveChanges();
 
-                    db.SaveChanges();
+					sysMsg.SystemMessage = "Ticket created Successfully!";
+					return request.CreateResponse(HttpStatusCode.Created, sysMsg);
+				}
 
-                    return HttpStatusCode.Accepted;
-                }
+				sysMsg.SystemMessage = "Request malformed!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+			else
+			{
+				sysMsg.SystemMessage = "Site key provided not recognized!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+		}
 
-                return HttpStatusCode.BadRequest;
-            }
-            else
-            {
-                return HttpStatusCode.BadRequest;
-            }
-        }
+		[HttpPut]
+		[Route("tickets/{siteId}/{clienteId}/ticket/{ticketId}")]
+		[ResponseType(typeof(SystemMessages))]
+		public HttpResponseMessage AddMsgToTicket(HttpRequestMessage request, string siteId, string clienteId, string ticketId, Messages msg)
+		{
+			SystemMessages sysMsg = new SystemMessages();
 
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				var tktInDb = db.TicketsDb.Include(m => m.MessagesList)
+											.Where(t => t.TicketId == ticketId)
+											.Where(t => t.SiteId == siteId)
+											.FirstOrDefault(a => a.ClienteId == clienteId);
 
+				if (tktInDb == null)
+				{
+					sysMsg.SystemMessage = "ClienteId or TicketId not found!";
+					return request.CreateResponse(HttpStatusCode.NotFound, sysMsg);
+				}
 
-        /*
+				if (tktInDb.StatusId == Status.Closed || tktInDb.StatusId == Status.Canceled)
+				{
+					sysMsg.SystemMessage = "Cannot change a resolved ticket!";
+					return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+				}
 
-            {
-	            sys_message: <string>
-            }
-            (DELETE)~/tickets/<CPF>/id/<ticket_ID> -> precisaria de uma mensagem ou item no link para saber se é CANCEL ou CLOSE
-         */
+				if (ModelState.IsValid)
+				{
+					tktInDb.MessagesList.Add(msg);
 
-    }
+					db.MessagesDb.Add(msg);
+					db.SaveChanges();
+
+					sysMsg.SystemMessage = "Message added successfully!";
+
+					return request.CreateResponse(HttpStatusCode.OK, sysMsg);
+				}
+
+				sysMsg.SystemMessage = "Request is malformed!";
+
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+			else
+			{
+				sysMsg.SystemMessage = "Site key provided not recognized!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+		}
+
+		[HttpDelete]
+		[Route("tickets/{siteId}/{clienteId}/ticket/{ticketId}")]
+		[ResponseType(typeof(SystemMessages))]
+		public HttpResponseMessage ResolveTicket(HttpRequestMessage request, string siteId, string clienteId, string ticketId, Messages msg, Status code)
+		{
+			SystemMessages sysMsg = new SystemMessages();
+
+			if (db.SiteResitersDb.FirstOrDefault(s => s.SiteKey == siteId) != null)
+			{
+				if (Enum.IsDefined(typeof(Status), code))
+				{
+					sysMsg.SystemMessage = "Invalid code!";
+					return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+				}
+
+				var tktInDb = db.TicketsDb.Include(m => m.MessagesList)
+											.Where(t => t.TicketId == ticketId)
+											.Where(t => t.SiteId == siteId)
+											.FirstOrDefault(a => a.ClienteId == clienteId);
+
+				if (tktInDb == null)
+				{
+					sysMsg.SystemMessage = "ClienteId or TicketId not found!";
+					return request.CreateResponse(HttpStatusCode.NotFound, sysMsg);
+				}
+
+				if (tktInDb.StatusId == Status.Closed || tktInDb.StatusId == Status.Canceled)
+				{
+					sysMsg.SystemMessage = "Cannot change a resolved ticket!";
+					return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+				}
+
+				if (ModelState.IsValid)
+				{
+					tktInDb.MessagesList.Add(msg);
+					tktInDb.StatusId = code;
+
+					db.MessagesDb.Add(msg);
+					db.SaveChanges();
+
+					sysMsg.SystemMessage = "Message added successfully!";
+
+					return request.CreateResponse(HttpStatusCode.OK, sysMsg);
+				}
+
+				sysMsg.SystemMessage = "Request is malformed!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+			else
+			{
+				sysMsg.SystemMessage = "Site key provided not recognized!";
+				return request.CreateResponse(HttpStatusCode.BadRequest, sysMsg);
+			}
+		}
+	}
 }
---
+++ APICentralAtendimento/CentralAtendimento/CentralAtendimento/Models/SystemMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentralAtendimento.Models
+{
+    public class SystemMessages
+    {
+        public string SystemMessage { get; set; }
+    }
+}
